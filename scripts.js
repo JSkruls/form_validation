969a879c90cbab81dda3d0b704e977f4e8dccf5d
JSkruls <62695130+JSkruls@users.noreply.github.com>
2025-03-11 16:52:48 +0200

    Add input validity check and error msgs on submit On form submit invalid fields would display default error messages, but on input key stroke they would be custom. Now they all are custom.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const inputs = document.querySelectorAll('input'); //node list of inputs
-const submitBtn = document.querySelector('#submit'); //create account button
+const submit = document.querySelector('#submit'); //create account button
 const nameRegex = /^[a-zA-Z]+$/; //name and surname validation pattern
 const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; //email validation pattern
 const phoneRegex = /^(\+)?(\d{3})?\d{8}$/; //phone validation pattern
@@ -15,11 +15,21 @@ const invalidityMessages = { // List of error msgs for each input type/name
   terms: 'Agree to terms to proceed',
 }
 
+// Check validity and set error msgs on each input
 inputs.forEach((input) => {
   input.addEventListener('input', (e) => {
     input.setCustomValidity(''); // Reset validation error & invalidity state 
     if(!input.checkValidity()) {
       input.setCustomValidity(invalidityMessages[input.name]); // Set error message as per object
+    }
+  });
+});
+
+// Check validity and set error msgs on from submit
+submit.addEventListener('click', (e) => {
+  inputs.forEach((input) => {
+    if(!input.checkValidity()) {
+      input.setCustomValidity(invalidityMessages[input.name]); 
     }
   });
 });
