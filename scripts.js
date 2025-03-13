64e5d1cc25f8ea240a556d9a82edba26961243f2
JSkruls <62695130+JSkruls@users.noreply.github.com>
2025-03-13 15:21:22 +0200

    Reworked paintIconRed() function Prior it looked for either 'fill' or 'stroke' prop inside style attribute before changing the color. Now it changes the color without finding out the property name. It works because each svg shape has only one instance of color inside it's style.

--- scripts.js
+++ scripts.js
@@ -5,7 +5,8 @@ const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; //email validation patter
 const phoneRegex = /^(\+)?(\d{3})?\d{8}$/; //phone validation pattern
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W])[A-Za-z\d\W]{6,15}$/; //password validation pattern
 
-const invalidityMessages = { // List of error msgs for each input type/name
+// List of error msgs for each input type/name
+const invalidityMessages = { 
   first_name: 'Enter your first name',
   surname: 'Enter your last name',
   email: 'Provide your email',
@@ -44,31 +45,30 @@ inputs.forEach((input) => {
 });
 
 //Add 'red' keys to object to switch from red back to green
-//Possible will need separate function for styleIconGreen
+//Possibly will need separate function for styleIconGreen
 const shapeColors = {
-  'fill: rgb(31, 52, 21)': 'rgb(52, 26, 21)',
-  'fill: rgb(141, 192, 116)': 'rgb(192, 129, 116)',
-  'stroke: rgb(31, 52, 21)': 'rgb(52, 26, 21)',
-  'stroke: rgb(141, 192, 116)': 'rgb(192, 129, 116)',
+  'rgb(31, 52, 21)': 'rgb(52, 26, 21)',
+  'rgb(141, 192, 116)': 'rgb(192, 129, 116)',
+  'rgb(31, 52, 21)': 'rgb(52, 26, 21)',
+  'rgb(141, 192, 116)': 'rgb(192, 129, 116)',
 }
-
-// Apply invalid colors to icon of current input field 
-// Works if svg child has either 'stroke' or 'fill', not both
+// Apply invalid colors to icon of current input field
 function paintIconRed(input) { 
   let shapes = input.parentElement.children[0].childNodes; // Grab all children/shapes of svg 
   shapes = Array.from(shapes).filter(shape => shape.nodeType !== Node.TEXT_NODE); // Remove text nodes for .getAttribute() to work
   shapes.forEach(shape => { 
-    let objectKey; 
-    if(Object.keys(shapeColors).some(key => { // Find and store object key matching inline css property 
-      objectKey = key; 
+    let objectKey;
+    console.log('new shape');
+    if(Object.keys(shapeColors).some(key => { // Find which inline rgb() value matches object's key 
+      objectKey = key; // Stores inline rgb() value to be changed
+      console.log(objectKey);
       return shape.getAttribute('style').includes(key); 
     })) { 
-      objectKey.includes('fill') ? // Change color of css prop found in style attribute
-        shape.style.fill = shapeColors[objectKey]: 
-        shape.style.stroke = shapeColors[objectKey]; 
+      shape.setAttribute("style", // Change inline color for one inside object
+        shape.getAttribute("style").replace(/rgb\(\s*\d+,\s*\d+,\s*\d+\s*\)/, 
+        shapeColors[objectKey]));
       }
   });
 }
-
 
 
