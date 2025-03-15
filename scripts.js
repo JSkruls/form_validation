cd19a4daeb1ca2f91f64a328bc294fae5c0c058f
JSkruls <62695130+JSkruls@users.noreply.github.com>
2025-03-15 12:43:33 +0200

    Add paintPasswordBars() function Before bars were painted via a lengthy switch statement, now it's done with one color object and two painting loops - gray and color.

--- scripts.js
+++ scripts.js
@@ -3,7 +3,10 @@ const submit = document.querySelector('#submit');
 const nameRegex = /^[a-zA-Z]+$/; //name and surname validation pattern
 const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; //email validation pattern
 const phoneRegex = /^(\+)?(\d{3})?\d{8}$/; //phone validation pattern
-const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W])[A-Za-z\d\W]{6,15}$/; //password validation pattern
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W])[A-Za-z\d\W]{4,10}$/; //password validation pattern
+const password = document.querySelector('input[name="password"]'); //password input
+const passTwo = document.querySelector('input[name="confirm"]'); //password confirmation input
+const passwordBars = document.querySelectorAll('.bar');
 
 // List of error msgs for each input type/name
 const invalidityMessages = { 
@@ -83,4 +86,49 @@ function paintIcon(input) {
         objectKey));
       }
   });
+}
+
+// Show progress bars on input 'focus'
+password.addEventListener('focus', (e) => { 
+  passwordBars.forEach((bar) => {
+    bar.style.visibility = 'visible';
+  });
+});
+
+// Hide progress bars on input 'blur'
+password.addEventListener('blur', (e) => { 
+  passwordBars.forEach((bar) => {
+    bar.style.visibility = 'hidden';
+  });
+});
+
+// Color bars inside password field based on regex patterns passed
+function paintPasswordBars(password) { 
+  const regexArray = ['[a-z]','[A-Z]','[0-9]','[!@#$%^&*()]'];
+  const passwordColors = [
+    'rgba(147, 147, 147, 1)',
+    'rgba(200, 25, 25, 1)',
+    'rgba(255, 172, 29, 1)',
+    'rgba(231, 229, 21, 1)',
+    'rgba(113, 183, 46, 1)'
+  ]
+  let colorIndex = 0; // Define the color for painted password bars
+
+  // Update password strength(colorIndex) on each input
+  regexArray.forEach(pattern => { 
+    if(new RegExp(pattern).test(password)) {
+        colorIndex++; 
+      }
+  });
+
+  // Grey out all password bars when backspacing password
+  // Must stay above for loop to work properly
+  passwordBars.forEach((bar) => { // just use passwordBars; hide bars instead of progress; delete progress querySelector
+    bar.style.background = 'rgba(147, 147, 147, 1)';
+  });
+
+  // Re-paint the number of bars equal to password strength(colorIndex)
+  for(i = 0; i < colorIndex; i++) {
+    passwordBars.item(i).style.backgroundColor = passwordColors[colorIndex];
+  }
 }
